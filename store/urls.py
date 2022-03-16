0b0bb27ac7296aa074191d5a334aa5b9e54e0a74
Festus Kwafo <festuskwafo@gmail.com>
2022-03-16 09:16:13 +0000

    clear

--- store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = 'store'
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home' ),
-    path('data-json/', ProductView.as_view(), name='data-json')
-]
