875ef2ea95144d6d5fd61b7c56918d72fe3fc463
Festus Kwafo <68119922+Festus-Kwafo@users.noreply.github.com>
2022-06-02 16:25:52 +0000

    google

--- appointment/views.py
+++ appointment/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from .forms import EventForm
+from bootstrap_datepicker_plus.widgets import DateTimePickerInput
+from django.views import generic
+from .models import Appointment
+class CreateView(generic.edit.CreateView):
+    model = Appointment
+    fields = ['name', 'email', 'phone_number', 'time_slot', 'note']
+    def get_form(self):
+        form = super().get_form()
+        form.fields['time_slot'].widget = DateTimePickerInput()
+        return form
 # Create your views here.
 def appointment(request):
-    return render(request, 'appointment.html')
+    forms = EventForm()
+    context = {
+        'forms': forms
+    }
+    return render(request, 'appointment.html', context )
