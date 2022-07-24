cf1891eed8ad244b236880768726dd00b2171dfc
10821161 <fokwafo001@gmail.com>
2022-07-24 13:47:33 +0000

    debug and mediafiles

--- backend/settings.py
+++ backend/settings.py
@@ -17,6 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
 ALLOWED_HOSTS = ['*']
 
 
--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import url
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
@@ -12,6 +14,7 @@ urlpatterns = [
     path('account/', include('accounts.urls', namespace='accounts')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('appointment/', include('appointment.urls', namespace='appointment')),
+    path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
